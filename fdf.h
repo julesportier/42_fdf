a4316093f1f6b1d11f6692a95abdacc51bacf153
Jules Portier <juportie@z2r9p1.42lyon.fr>
2025-01-27 15:34:23 +0100

    feat: functions to put pixels and squares to the screen

--- fdf.h
+++ fdf.h
@@ -6,11 +6,31 @@
 /*   By: juportie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 11:11:59 by juportie          #+#    #+#             */
-/*   Updated: 2025/01/27 11:13:28 by juportie         ###   ########.fr       */
+/*   Updated: 2025/01/27 15:33:28 by juportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minilibx/mlx.h"
-// !!! DEBUG >>>
-#include <stdio.h>
-// <<< DEBUG !!!
+#ifndef FDF_H
+# define FDF_H
+
+# include "libft/libft.h"
+# include "libft/ftpf_printf.h"
+# include "minilibx/mlx.h"
+
+# define WIDTH 1920
+# define HEIGHT 1080
+
+typedef struct	s_img_data {
+	void	*img;
+	char	*addr;
+	int	bits_per_pixel;
+	int	line_length;
+	int	endian;
+}	t_img_data;
+
+typedef struct	s_pos {
+	int	x;
+	int	y;
+}	t_pos;
+
+#endif
--- test.c
+++ test.c
@@ -6,24 +6,35 @@
 /*   By: juportie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 10:33:57 by juportie          #+#    #+#             */
-/*   Updated: 2025/01/27 11:13:54 by juportie         ###   ########.fr       */
+/*   Updated: 2025/01/27 15:33:44 by juportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minilibx/mlx.h"
-#include <stdio.h>
+#include "fdf.h"
 
-typedef struct	s_img_data {
-	void	*img;
-	char	*addr;
-	int	bits_per_pixel;
-	int	line_length;
-	int	endian;
-}	t_img_data;
+/*
+Input coordinates:
+----------
+|    1   |
+|-1  0  1|
+|   -1   |
+----------
+*/
+static t_pos	scale_pixel(double x, double y, int width, int height)
+{
+	t_pos	pixel;
 
-static void	pixel_put(t_img_data *img_data, int x, int y, int color)
+	x = (x + 1) * 0.5;
+	y = (y + 1) * 0.5;
+	pixel.x = x * width;
+	pixel.y = y * height;
+	return (pixel);
+}
+
+static void	put_pixel(t_img_data *img_data, int x, int y, int color)
 {
 	char	*pix_addr;
+
 
 	pix_addr = img_data->addr + (
 		y * img_data->line_length + x * (img_data->bits_per_pixel / 8)
@@ -31,34 +42,68 @@ static void	pixel_put(t_img_data *img_data, int x, int y, int color)
 	*(unsigned int *)pix_addr = color;
 }
 
+static void	put_square(t_img_data *img_data, int x, int y, int color, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			if (x + i <= WIDTH && y + j <= HEIGHT)
+				put_pixel(img_data, x + i, y + j, color);
+			if (x + i <= WIDTH && y - j <= HEIGHT)
+				put_pixel(img_data, x + i, y - j, color);
+			if (x - i <= WIDTH && y + j <= HEIGHT)
+				put_pixel(img_data, x - i, y + j, color);
+			if (x - i <= WIDTH && y - j <= HEIGHT)
+				put_pixel(img_data, x - i, y - j, color);
+			j++;
+		}
+		i++;
+	}
+}
+
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
 	t_img_data	img_data;
+	t_pos	pixel;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "HELLO");
-	img_data.img = mlx_new_image(mlx, 1920, 1080);
+	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "HELLO");
+	img_data.img = mlx_new_image(mlx, WIDTH, HEIGHT);
 	img_data.addr = mlx_get_data_addr(
 		img_data.img,
 		&img_data.bits_per_pixel,
 		&img_data.line_length,
 		&img_data.endian
 	);
-	//printf("bits_per_pixel == %d\n", img_data.bits_per_pixel);
-	//printf("line_length == %d\n", img_data.line_length);
-	//printf("endian == %d\n", img_data.endian);
-	//pixel_put(&img_data, 150, 150, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img_data.img, 0, 0);
+	//ft_printf("bits_per_pixel == %d\n", img_data.bits_per_pixel);
+	//ft_printf("line_length == %d\n", img_data.line_length);
+	//ft_printf("endian == %d\n", img_data.endian);
+	double	x = -0.5;
+	double	y = 0.5;
 	int	i = 0;
-	while (i < 100)
+	while (i < 2)
 	{
-		pixel_put(&img_data, 19 * i, 150, 0x00FF0000);
-		pixel_put(&img_data, 19 * i + 1, 150, 0x00FF0000);
-		pixel_put(&img_data, 19 * i, 151, 0x00FF0000);
-		pixel_put(&img_data, 19 * i + 1, 151, 0x00FF0000);
+		int	j = 0;
+		while (j < 2)
+		{
+			pixel = scale_pixel(x + j, y - i, WIDTH, HEIGHT);
+			ft_printf("x == %d; y == %d\n", pixel.x, pixel.y);
+			//put_pixel(&img_data, pixel.x + j, pixel.y - i, 0x00FF0000);
+			put_square(
+				&img_data, pixel.x, pixel.y, 0x00FF0000, 7
+			);
+			j++;
+		}
 		i++;
 	}
+	mlx_put_image_to_window(mlx, mlx_win, img_data.img, 0, 0);
 	mlx_loop(mlx);
 }
